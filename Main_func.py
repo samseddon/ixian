53f4c5b9df941863091c49689d25913bfa1882dd
sseddon <samuel.seddon@gmail.com>
2022-09-22 17:19:14 +0200

    functions galore

--- Main_func.py
+++ Main_func.py
@@ -5,48 +5,77 @@ import numpy as np
 from nexusformat import nexus
 from useful_functions import progress_bar,existential_check
 from Param_dict import param
+from equations import *
 
-def index_grid(Param_dict):  # This is the grid that keeps track of how mnay elements are being put into each Q voxel.
-    # Identical size to Q volume
-    idx_grid = np.zeros((Param_dict['nr_pts_x'], Param_dict['nr_pts_y'], Param_dict['nr_pts_z']))
-    return idx_grid
+def voxel_fill(i,j,attenuator,two_theta_range_new,omega,wavelength,two_theta_h_range,x_slope, x_intercept,y_slope,y_intercept,z_slope,z_intercept):
 
-# Makes a list of 2thetas corresponding to each row of pixels (top down) in an image
-def generate_two_thetas(TwoTheta0, Param_dict):
-    two_thetas = []
-    for x in range(Param_dict['ShotHeight']):
-        two_theta = TwoTheta0 + (Param_dict['Zero_Pixel_Ver'] - float(x)) * 0.01126  # degrees per row determined from alignment calibration (pixel # vs. two theta)
-        two_thetas.append(two_theta)
-    return two_thetas
+    fqx = calc_qx(two_theta_range_new[j], omega, wavelength, two_theta_h_range[i])
+    fqy = calc_qy(two_theta_range_new[j], omega, wavelength, two_theta_h_range[i])
+    fqz = calc_qz(two_theta_range_new[j], omega, wavelength)
+    if attenuator == 0:
 
-# Calculate qx from 2theta, eta, and wavelength
-def calc_qx(twoTheta, eta, wvl, angle_y):
-    qx = (2 * np.pi / wvl) * (np.cos(eta / 180 * np.pi) - np.cos(twoTheta / 180 * np.pi - eta / 180 * np.pi)
-                              * np.cos(angle_y / 180 * np.pi))
-    return qx
+        if int(fqx * x_slope + x_intercept) > number_x \
+                or int(fqy * y_slope + y_intercept) > number_y \
+                or int(fqz * z_slope + z_intercept) > number_z \
+                or int(fqx * x_slope + x_intercept) < 0 \
+                or int(fqy * y_slope + y_intercept) < 0 \
+                or int(fqz * z_slope + z_intercept) < 0:
+
+            # skipped = skipped+1
+            pass
+        else:
+            if f_new[j, i] * io * 10 ** -6 > (sat_pix):  # 1.08*10**6
+                print('hotspot')
+                # hotspot=hotspot+1
+                pass
+            else:
+                q_3d[int(fqx * x_slope + x_intercept),
+                     int(fqy * y_slope + y_intercept),
+                     int(fqz * z_slope + z_intercept)] \
+                    = q_3d[int(fqx * x_slope + x_intercept),
+                           int(fqy * y_slope + y_intercept),
+                           int(fqz * z_slope + z_intercept)] + f_new[j, i]
+                idx_grid[int(fqx * x_slope + x_intercept),
+                         int(fqy * y_slope + y_intercept),
+                         int(fqz * z_slope + z_intercept)] \
+                    = idx_grid[int(fqx * x_slope + x_intercept),
+                               int(fqy * y_slope + y_intercept),
+                               int(fqz * z_slope + z_intercept)] + 1
 
 
-# Calculate qz from 2theta, eta, and wavelength
-def calc_qz(twoTheta, eta, wvl):
-    qz = (2 * np.pi / wvl) * (np.sin(eta / 180 * np.pi) + np.sin(twoTheta / 180 * np.pi - eta / 180 * np.pi))
-    return qz
+    if attenuator != 0 and f_new[j, i] < 1000:  # T.H. Made 1000 from 100
+        # low_count = low_count+1
+        pass
+    if attenuator != 0 and f_new[j, i] > 1000:
+        if f_new[j, i] * io * 10 ** -6 > (sat_pix):
+            print('hotspot')
+            # hotspot= hotspot + 1
+            pass
+        else:
+            if int(fqx * x_slope + x_intercept) > number_x \
+                    or int(fqy * y_slope + y_intercept) > number_y \
+                    or int(fqz * z_slope + z_intercept) > number_z \
+                    or int(fqx * x_slope + x_intercept) < 0 \
+                    or int(fqy * y_slope + y_intercept) < 0 \
+                    or int(fqz * z_slope + z_intercept) < 0:
 
+                # skipped = skipped + 1
+                pass
+            else:
+                q_3d[int(fqx * x_slope + x_intercept),
+                     int(fqy * y_slope + y_intercept),
+                     int(fqz * z_slope + z_intercept)] \
+                    = q_3d[int(fqx * x_slope + x_intercept),
+                           int(fqy * y_slope + y_intercept),
+                           int(fqz * z_slope + z_intercept)] + (f_new[j, i] / trans)
+                idx_grid[int(fqx * x_slope + x_intercept),
+                         int(fqy * y_slope + y_intercept),
+                         int(fqz * z_slope + z_intercept)] \
+                    = idx_grid[int(fqx * x_slope + x_intercept),
+                               int(fqy * y_slope + y_intercept),
+                               int(fqz * z_slope + z_intercept)] + 1
 
-# Calculate qy from 2theta, eta, and wavelength
-def calc_qy(twoTheta, eta, wvl, angle_y):
-    qy = (2 * np.pi / wvl) * np.sin(angle_y / 180 * np.pi) \
-         * np.cos(twoTheta / 180 * np.pi - eta / 180 * np.pi)
-    return qy
-
-
-def qy_angle(offset, Param_dict):
-    qy_angles = []
-    for x in range(Param_dict['ShotHeight_y']):
-        angle = (Param_dict['Zero_Pixel_Hor'] - offset - float(x)) * 0.01126  
-        # degrees per column determined from alignment calibration (pixel # vs. nu)
-        qy_angles.append(angle)
-    return qy_angles
-
+    return q_3d, idx_grid
 
 def q_array_3d(Param_dict):
 
@@ -87,21 +116,6 @@ def q_array_3d(Param_dict):
     fqy = np.linspace(qy_min, qy_min + delta_qy_range, nr_pts_y)
 
     return q_3d, x_slope, y_slope, z_slope, z_intercept, x_intercept, y_intercept, fqx, fqy, fqz
-
-def dead_pixel(f,param,two_theta_range):
-    #  REMOVE THE DEAD PIXELS
-    DeadRow1 = param['DeadRow1']   
-    # Defines the Dead regions on the Pilatus where the chips are connected - 300k so only in rows!   #######
-    DeadRow2 = param['DeadRow2']
-    DeadRow3 = param['DeadRow3']
-    DeadRow4 = param['DeadRow4']
-    f_new = np.concatenate((f.data[:DeadRow1 - 1, :],
-                        f.data[DeadRow2 + 1:DeadRow3 - 1, :],
-                        f.data[DeadRow4 + 1:, :]), axis=0)
-    two_theta_range_new = np.concatenate((two_theta_range[:DeadRow1 - 1],
-                                          two_theta_range[DeadRow2 + 1:DeadRow3 - 1],
-                                          two_theta_range[DeadRow4 + 1:]))
-    return f_new,two_theta_range_new
 
 #print(param)
 directory="/home/sseddon/Desktop/500GB/Data/XMaS/magnetite/data"
@@ -147,71 +161,8 @@ for k in range(len(master_files)):
     f_new = (f_new / io) * 10 ** 6
     for i in range(f_new.shape[1]):
         for j in range(f_new.shape[0]):  # FIRST ITERATE THE ROWS A.K.A TWO THETA
-            fqx = calc_qx(two_theta_range_new[j], omega, wavelength, two_theta_h_range[i])
-            fqy = calc_qy(two_theta_range_new[j], omega, wavelength, two_theta_h_range[i])
-            fqz = calc_qz(two_theta_range_new[j], omega, wavelength)
-            if attenuator == 0:
-
-                if int(fqx * x_slope + x_intercept) > number_x \
-                        or int(fqy * y_slope + y_intercept) > number_y \
-                        or int(fqz * z_slope + z_intercept) > number_z \
-                        or int(fqx * x_slope + x_intercept) < 0 \
-                        or int(fqy * y_slope + y_intercept) < 0 \
-                        or int(fqz * z_slope + z_intercept) < 0:
-
-                    # skipped = skipped+1
-                    pass
-                else:
-                    if f_new[j, i] * io * 10 ** -6 > (sat_pix):  # 1.08*10**6
-                        print('hotspot')
-                        # hotspot=hotspot+1
-                        pass
-                    else:
-                        q_3d[int(fqx * x_slope + x_intercept),
-                             int(fqy * y_slope + y_intercept),
-                             int(fqz * z_slope + z_intercept)] \
-                            = q_3d[int(fqx * x_slope + x_intercept),
-                                   int(fqy * y_slope + y_intercept),
-                                   int(fqz * z_slope + z_intercept)] + f_new[j, i]
-                        idx_grid[int(fqx * x_slope + x_intercept),
-                                 int(fqy * y_slope + y_intercept),
-                                 int(fqz * z_slope + z_intercept)] \
-                            = idx_grid[int(fqx * x_slope + x_intercept),
-                                       int(fqy * y_slope + y_intercept),
-                                       int(fqz * z_slope + z_intercept)] + 1
-
-
-            if attenuator != 0 and f_new[j, i] < 1000:  # T.H. Made 1000 from 100
-                # low_count = low_count+1
-                pass
-            if attenuator != 0 and f_new[j, i] > 1000:
-                if f_new[j, i] * io * 10 ** -6 > (sat_pix):
-                    print('hotspot')
-                    # hotspot= hotspot + 1
-                    pass
-                else:
-                    if int(fqx * x_slope + x_intercept) > number_x \
-                            or int(fqy * y_slope + y_intercept) > number_y \
-                            or int(fqz * z_slope + z_intercept) > number_z \
-                            or int(fqx * x_slope + x_intercept) < 0 \
-                            or int(fqy * y_slope + y_intercept) < 0 \
-                            or int(fqz * z_slope + z_intercept) < 0:
-
-                        # skipped = skipped + 1
-                        pass
-                    else:
-                        q_3d[int(fqx * x_slope + x_intercept),
-                             int(fqy * y_slope + y_intercept),
-                             int(fqz * z_slope + z_intercept)] \
-                            = q_3d[int(fqx * x_slope + x_intercept),
-                                   int(fqy * y_slope + y_intercept),
-                                   int(fqz * z_slope + z_intercept)] + (f_new[j, i] / trans)
-                        idx_grid[int(fqx * x_slope + x_intercept),
-                                 int(fqy * y_slope + y_intercept),
-                                 int(fqz * z_slope + z_intercept)] \
-                            = idx_grid[int(fqx * x_slope + x_intercept),
-                                       int(fqy * y_slope + y_intercept),
-                                       int(fqz * z_slope + z_intercept)] + 1
+            q_3d,idx_grid = voxel_fill(i,j,attenuator,two_theta_range_new,omega,wavelength,two_theta_h_range,
+                    x_slope, x_intercept,y_slope,y_intercept,z_slope,z_intercept)
 
     f.close()
 
--- equations.py
+++ equations.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Calculate qx from 2theta, eta, and wavelength
 def calc_qx(twoTheta, eta, wvl, angle_y):
     qx = (2 * np.pi / wvl) * (np.cos(eta / 180 * np.pi) - np.cos(twoTheta / 180 * np.pi - eta / 180 * np.pi)
@@ -16,4 +18,42 @@ def calc_qy(twoTheta, eta, wvl, angle_y):
     qy = (2 * np.pi / wvl) * np.sin(angle_y / 180 * np.pi) \
          * np.cos(twoTheta / 180 * np.pi - eta / 180 * np.pi)
     return qy
+
+def index_grid(Param_dict):  # This is the grid that keeps track of how mnay elements are being put into each Q voxel.    # Identical size to Q volume
+    idx_grid = np.zeros((Param_dict['nr_pts_x'], Param_dict['nr_pts_y'], Param_dict['nr_pts_z']))
+    return idx_grid
+
+# Makes a list of 2thetas corresponding to each row of pixels (top down) in an image
+def generate_two_thetas(TwoTheta0, Param_dict):
+    two_thetas = []
+    for x in range(Param_dict['ShotHeight']):
+        two_theta = TwoTheta0 + (Param_dict['Zero_Pixel_Ver'] - float(x)) * 0.01126  # degrees per row determined from alignment calibration (pixel # vs. two theta)
+        two_thetas.append(two_theta)
+    return two_thetas
+
+
+
+def qy_angle(offset, Param_dict):
+    qy_angles = []
+    for x in range(Param_dict['ShotHeight_y']):
+        angle = (Param_dict['Zero_Pixel_Hor'] - offset - float(x)) * 0.01126
+        # degrees per column determined from alignment calibration (pixel # vs. nu)
+        qy_angles.append(angle)
+    return qy_angles
+
+def dead_pixel(f,param,two_theta_range):
+    #  REMOVE THE DEAD PIXELS
+    DeadRow1 = param['DeadRow1']
+    # Defines the Dead regions on the Pilatus where the chips are connected - 300k so only in rows!   #######
+    DeadRow2 = param['DeadRow2']
+    DeadRow3 = param['DeadRow3']
+    DeadRow4 = param['DeadRow4']
+    f_new = np.concatenate((f.data[:DeadRow1 - 1, :],
+                        f.data[DeadRow2 + 1:DeadRow3 - 1, :],
+                        f.data[DeadRow4 + 1:, :]), axis=0)
+    two_theta_range_new = np.concatenate((two_theta_range[:DeadRow1 - 1],
+                                          two_theta_range[DeadRow2 + 1:DeadRow3 - 1],
+                                          two_theta_range[DeadRow4 + 1:]))
+    return f_new,two_theta_range_new
+
 
