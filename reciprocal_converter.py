5323ffc1d6c333a35bd081be0e7884285cf0bbb2
sseddon <samuel.seddon@gmail.com>
2022-11-22 16:31:46 +0100

    spot_dict now added, and needs to be a txt file

--- reciprocal_converter.py
+++ reciprocal_converter.py
@@ -6,7 +6,6 @@ from nexusformat import nexus
 from useful_functions import progress_bar,existential_check
 #from Param_dict import param
 from equations import *
-from spot_dict import spot_dict
 from joblib import Parallel, delayed
 import multiprocessing as mp 
 import pickle
@@ -184,6 +183,9 @@ def data_fill(directory,output_folder,file_reference,scan_num):
     files_location = os.listdir(directory)
     master_files = [m for m in files_location if m.startswith(file_reference) and m.endswith(".edf")]
     master_files = [c for c in master_files if int(c.split("_")[-2]) in scan_num]
+    with open(directory[:-4]+'user_defined_parameters/spot_dict.txt','r') as inf:
+        spot_dict = eval(inf.read())
+    print(spot_dict)
     param = param_read(spot_dict,scan_num[0],directory)
     q_3d, x_slope, y_slope, z_slope, z_intercept, x_intercept, y_intercept, qx, qy, qz = q_array_3d(param,
             spot_dict,scan_num[0],directory)
